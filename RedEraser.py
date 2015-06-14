bbb2cd65ae78833a1b4fb594cab5392370125472
JJ <joshuajordanlowe@gmail.com>
2015-06-14 16:04:21 -0600

    Comments and cleanup
    -Added lots of comments for the less saavy to at least have a high level working of what's going on
    -Increased comment and post high end to 100 billion and also allowed users to specify how many comments and/or posts they'd like deleted
    -Added some try catches around login and the post/comment deleters. It doesn't handle any of the errors just prints them out right now. Things like bad passwords are pretty descriptive while things like Reddit's servers being down are not. 
    -Modulized everything to make it look like better code
    
    -Disappointed that my 25 line program is now over 125 lines because of functions and comments

--- RedEraser.py
+++ RedEraser.py
@@ -1,33 +1,129 @@
 #!python2
- 
+
 import praw
- 
-USERNAME  = "Username"
-PASSWORD  = "Password"
+
+# Enter your username and password below
+USERNAME  = "YOUR USERNAME"
+PASSWORD  = "YOUR PASSWORD"
+
+# Leave this as -1 if you want to erase everything
+# Otherwise change to how many comments or posts you want deleted
+commentsToDelete = -1
+postsToDelete = -1
+
+'''--------------------------------------------------------'''
+#                                                            #
+# Generally the things below this line should not be touched #
+#                                                            #
+'''--------------------------------------------------------'''
+
+# Leave the useragent as is
 USERAGENT = "Comment Deleter"
- 
-r = praw.Reddit(USERAGENT)
-r.login(USERNAME, PASSWORD)
- 
-user = r.get_redditor(USERNAME)
- 
-for o in xrange(0,999):
-    cc = 0
-    pp = 0    
- 
-    for c in user.get_comments(limit=None):
-        c.edit('#')
-        c.delete()
-        print 'Pass: ' + str(o) + ' ; Iteration: ' + str(cc) + ' ; Comment Deleted'
-        cc += 1
- 
-    print str(cc) + ' Comments Deleted'
- 
-    for p in user.get_submitted(limit=None):
-        if p.selftext:
-            p.edit('#')
-        p.delete()
-        print 'Pass: ' + str(o) + ' ; Iteration: ' + str(pp) + ' ; Post Deleted'
-        pp += 1
- 
-    print str(pp) + ' Posts Deleted'
+
+# This just tracks how many comments and posts have been deleted so far
+# Do not touch
+commentsDeleted = 0
+postsDeleted = 0
+
+# This simply logs you in and returns a user object
+def login():
+    r = praw.Reddit(USERAGENT)
+    r.login(USERNAME, PASSWORD)
+    user = r.get_redditor(USERNAME)
+
+    print "Successfully logged %s in" % USERNAME
+
+    return user
+
+# Takes the user object and deletes the users comments up to the amount
+# specified in commentsToDelete above
+def commentDeleter(user, commentsToDelete):
+
+    print "Deleting comments now"
+    
+    # Tracks how many comments have deleted so far
+    global commentsDeleted
+
+    # By accessing the 100 comment limit one billion times,
+    # it's safe to assume that this will delete any and all comments/posts (up to 100 billion)
+    for o in xrange(0,999999999):
+
+        # This is a simple check after one run through to see if there were any comments to delete
+        if o > 0 and commentsDeleted == 0:
+            print "No comments to delete!"
+            return
+
+        # Due to the limits on reddit's API, this for loop can only access 100 comments at a time
+        for c in user.get_comments(limit=None):
+
+            # Because reddit only saves the most recently edited comment, 
+            # each comment is edited with a pound symbol and then deleted
+            c.edit('#')
+            c.delete()
+            
+            commentsDeleted += 1
+            print "Comment Deleted"
+
+            # It checks if it's deleted your specified amount of comments,
+            # if left at '-1' this condition will never be met and it will delete all comments
+            if commentsDeleted == commentsToDelete:
+                print "{0} Comments Deleted".format(commentsDeleted)
+                return
+
+            # After every 10 comments it prints how many comments have been deleted so far
+            if commentsDeleted % 10 == 0:
+                print "{0} Comments Deleted".format(commensDeleted)
+
+    if commentsDeleted != 0:
+        print "All {0} Comments Deleted".format(postsDeleted)
+
+# This is almost identical to commentDeleter()
+def postDeleter(user, postsToDelete):
+
+    print "Deleting posts now"
+
+    global postsDeleted
+
+    for o in xrange(0,999999999):
+        
+        if o > 0 and postsDeleted == 0:
+            print "No posts to delete!"
+            return
+        
+        for p in user.get_submitted(limit=None):
+            if p.selftext:
+                p.edit('#')
+            p.delete()
+            postsDeleted = 0
+
+            if postsDeleted == postsToDelete:
+                print "{0} Posts Deleted".format(postsDeleted)
+                return
+
+            if postsDeleted % 10 == 0:
+                print "{0} Posts Deleted".format(postsDeleted)
+
+    if postsDeleted != 0:
+        print "All {0} Posts Deleted".format(postsDeleted)
+
+def main():
+    try:
+        user = login()
+    except Exception, e:
+        print "Login failed due to: "
+        print str(e)
+        return
+
+    try:
+        commentDeleter(user, commentsToDelete)
+        postDeleter(user, postsToDelete)
+    except Exception, e:
+        print "Deletion failed due to: "
+        print str(e)
+        return
+
+    print "All {0} Comments and {1} posts Deleted".format(commentsDeleted, postsDeleted)
+
+
+if __name__ == "__main__":
+    main()
